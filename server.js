d4700f7a5eeba672e2921b372299af77eaf299c2
JoanneOs <jowan.awsman@yahoo.com>
2025-04-09 14:36:32 -0500

    initiating project

---
+++ server.js
@@ -0,0 +1,11 @@
+// //here is how i set up my project inspired by my triway project;
+// mkdir TriWay-Project && cd TriWay-Project
+// npm init -y
+// npm install express ejs && npm install --save-dev nodemon
+// Create .gitignore and add node_modules
+// mkdir public views routes controllers models middleware data && mkdir public/css public/js views/partials routes/api
+// Create files: touch server.js public/css/style.css public/js/main.js views/*.ejs routes/*.js controllers/*.js models/*.js middleware/*.js data/*.json
+// Update package.json: "scripts": { "start": "node server.js", "dev": "nodemon server.js" }
+// git init
+// npm install
+// nodemon index.js or npm run dev for auto-restart
