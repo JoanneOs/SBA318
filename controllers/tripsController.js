a8066004f5a447a1713302458a64310b8c00f0a4
JoanneOs <jowan.awsman@yahoo.com>
2025-04-10 12:52:01 -0500

    put, delete trips

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,9 +1,17 @@
 // This one does the same thing but for TRIPS
 
+const fs = require('fs');
+const path = require('path');
+
 const trips = require('../data/trips.json'); // grab the trip data
 
 // Function to respond with all trip data
 const getTrips = (req, res) => {
+
+fs.writeFileSync(__dirname, '../data/trips.json',
+    json.stringify(trips, null, 2)
+);
+
   res.json(trips);
 };
 
